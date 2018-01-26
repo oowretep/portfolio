28a7abc64e0f3cd818128521209b184b1ee4a1d5
oowretep <13woopeter@gmail.com>
2018-01-26 04:24:03 -0800

    cleaning up files + working on applications developed + contact sections

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,5 +1,20 @@
-// $(document).ready(intializeApp);
+$(document).ready(function() {
+    var email = 'filleremail@filler.filler';
+    var phone = '123-456-7890'
 
-// function intializeApp() {
-// }
+    $('.phone').text(phone)
+    // .attr('href', 'tel:17147889553');
+    $('.email').text(email)
+    // .attr('href', 'mailto:'+email);
+    $('.stubbies.live').click(stubbiesLive);
+    $('.stubbies.code').click(stubbiesCode);
+});
 
+function stubbiesLive() {
+    console.log('live was pressed for stubbies');
+    window.open('https://www.google.com');
+}
+
+function stubbiesCode() {
+    window.open('https://www.github.com/oowretep');
+}
