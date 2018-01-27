1ac9089f086b00c2a42ea3e3591d17f649a1ef45
oowretep <13woopeter@gmail.com>
2018-01-27 02:43:28 -0800

    fixed resume links + finalizing applications developed section

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -4,15 +4,39 @@ $(document).ready(function() {
 
     $('.phone').text(phone).attr('href', 'tel:17147889553');
     $('.email').text(email).attr('href', 'mailto:'+email);
+    $('.resume-link').click(resumeLink);
     $('.stubbies.live').click(stubbiesLive);
     $('.stubbies.code').click(stubbiesCode);
+    $('.ticTac.live').click(ticTacLive);
+    $('.ticTac.code').click(ticTacCode);
+    $('.fridgeBridge.live').click(fridgeBridgeLive);
+    $('.fridgeBridge.code').click(FridgeBridgeCode);
 });
 
+function resumeLink() {
+    window.open('https://drive.google.com/file/d/1MlHtxETo-e4LhOLQvShNLQ4Kvd52Km23/view?usp=sharing');
+}
+
 function stubbiesLive() {
-    console.log('live was pressed for stubbies');
     window.open('https://www.google.com');
 }
 
 function stubbiesCode() {
     window.open('https://www.github.com/oowretep');
+}
+
+function ticTacLive() {
+    window.open('https://www.google.com');
+}
+
+function ticTacCode() {
+    window.open('https://github.com/oowretep/Tic-Tac-Duck-Hunt');
+}
+
+function fridgeBridgeLive() {
+    window.open('https://www.google.com');
+}
+
+function FridgeBridgeCode() {
+    window.open('https://github.com/oowretep/Fridge-Bridge');
 }
